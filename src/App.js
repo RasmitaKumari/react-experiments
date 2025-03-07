bcef1c45b23e3ef20143cf1eff7ca912adda117b
Rasmita Kumari <rasmi.sharma17@gmail.com>
2025-03-07 17:14:16 +0530

    Adding an counter application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import Counter from "./Counter";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, reset } from './Redux/slice';
 
-function App() {
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <Counter />
+      <h1>Counter: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
